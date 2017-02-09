23db92e3f533762d7ceaf73c8a27448a3f86189e
BrunoIF <bruno.i.fernandes@hotmail.com>
2017-02-09 11:23:07 -0200

    Orientação a Objetos - Introdução

---
+++ Classes/Classes/Aula1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classes
+{
+    public class Aula1
+    {
+        
+        public void Mensagem(string msg)
+        {
+            MessageBox.Show(msg + " :)");
+            //var form = new Form1();
+            //form.Text = "Testando";
+        }
+
+    }
+}
