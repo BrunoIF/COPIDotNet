c679d7efe7345d55134b5bb0dfcb4b6889f82f88
logonrm <logonrm@fiap.com.br>
2017-03-02 10:58:02 -0300

    Tratamento de exceptions / Mudança de evento

---
+++ ValidaCPF23023mia/ValidaCPF/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidaCPF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            // setar o focus
+            maskedTextBox1.Focus();
+            // mudar a imagem do picture box
+            picStatus.Image = Properties.Resources.alert;
+        }
+
+        private void maskedTextBox1_Leave(object sender, EventArgs e)
+        {
+            //capturando o conteúdo da masked textbox e armazenado na variável numcpf.
+            //atenção variáveis do tipo string não precisam do .tostring()
+            string numcpf = maskedTextBox1.Text;
+            //a linha abaixo elimina o . (ponto) e o - (hífen) 
+            numcpf = numcpf.Replace(".", "").Replace("-", "").Trim();
+            //MessageBox.Show(numcpf);
+
+            //antes de validar o cpf, trancar exceptions:
+            if ((numcpf.Length != 11) || (numcpf == "00000000000") || (numcpf == "11111111111") || (numcpf == "22222222222") || (numcpf == "33333333333") || (numcpf == "44444444444") || (numcpf == "55555555555") || (numcpf == "66666666666") || (numcpf == "77777777777") || (numcpf == "88888888888") || (numcpf == "99999999999"))
+            {
+                MessageBox.Show("Digite um CPF corretamente.");
+            }
+            else
+            {
+                //tratamento do retorno do método ValidaCPF
+                // o retorno do método é true ou false
+                //o caminho inicia com namespace -> Classe -> método
+                //se retorno for verdadeiro
+                if (ValidaCPF.ValidadorCPF.verificaCPF(numcpf)) //envia para a validação
+                {
+                    //MessageBox.Show("Válido");
+                    picStatus.Image = Properties.Resources.ok;
+                }
+                else
+                {
+                    //MessageBox.Show("Inválido");
+                    picStatus.Image = Properties.Resources.erro;
+                }
+            }
+        }
+    }
+}
--- ValidaCPF23023mia/ValidaCPF/Properties/AssemblyInfo.cs
+++ ValidaCPF23023mia/ValidaCPF/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c5ea4a47-e8d3-4430-8adc-f85f164311a8")]
+[assembly: Guid("f09019e8-ebb5-4848-8c0f-8b8c739a426d")]
 
 // Version information for an assembly consists of the following four values:
 //
