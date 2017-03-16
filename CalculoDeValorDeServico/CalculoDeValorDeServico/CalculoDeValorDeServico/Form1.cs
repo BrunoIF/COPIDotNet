63bd9ffd5c9a63de0db88bd50995fe7ad4fb80de
logonrm <logonrm@fiap.com.br>
2017-03-16 11:09:20 -0300

    Calculador de valor de serviço

--- CalculoDeValorDeServico/CalculoDeValorDeServico/CalculoDeValorDeServico/Form1.cs
+++ CalculoDeValorDeServico/CalculoDeValorDeServico/CalculoDeValorDeServico/Form1.cs
@@ -15,17 +15,34 @@ namespace CalculoDeValorDeServico
         public Form1()
         {
             InitializeComponent();
+            label5.Visible = false;
         }
 
         // Cálculo do valor do serviço
         // Fórmula adaptada do SEBRAE
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            // Algorítmo para calcular o serviço
-            // 1) Salário Líquido
-            // 2) Acrescentar 30% (impostos sobre SL)
-            // 3) Somar despesas fixas
-            // 4) Acrescentar 20% de investimento
+
+            try
+            {
+                // Algorítmo para calcular o serviço
+                // 1) Salário Líquido
+                // 2) Acrescentar 30% (impostos sobre SL)
+                // 3) Somar despesas fixas
+                // 4) Acrescentar 20% de investimento
+                // 5) Determinar total de horas/mês 
+                double salario = Convert.ToDouble(txtSalario.Text.Replace(",",".")), despesas = Convert.ToDouble(txtDespesas.Text.Replace(",", ".")), total = 0, horas = Convert.ToDouble(txtHoras.Text.Replace(",", "."));
+
+                // total = (SL + 30% de SL + despesas + 20% de investimento)/horas
+                total = (salario * 1.5 + despesas) / horas;
+                lblTotal.Text = total.ToString("c2");
+                label5.Visible = true;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Digite um valor válido.");
+            }
         }
     }
 }
