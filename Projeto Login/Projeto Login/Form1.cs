b4dd84ae7e008b3c4d8918540c73699a486a3ac5
BRUNO IROKAWA  FERNANDES <RM12294@fiap.com.br>
2017-03-23 10:37:46 -0300

    Tela de Login - CRUD com parâmetros

---
+++ Projeto Login/Projeto Login/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Projeto_Login
+{
+    public partial class Form1 : Form
+    {
+        static string strCn = "Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\DBLogin.mdf;Integrated Security = True";
+        SqlConnection conexao = new SqlConnection(strCn);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == String.Empty) || (textBox2.Text == String.Empty))
+            {
+                MessageBox.Show("É obrigatório o preenchimento dos campos Usuário e Senha", "Atenção !", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            else
+            {
+                // Uso de Parâmetros para seleção de usuário e senha do banco de dados
+                // @Nome do campo da tabela
+                string pesquisa = "select * from tbUsuarios where Login=@Login and Senha=@Senha";
+
+                SqlCommand cmd = new SqlCommand(pesquisa, conexao);
+                // Captura dos parâmetros
+                // @Login recebe o valor
+                cmd.Parameters.AddWithValue("@Login", textBox1.Text);
+                cmd.Parameters.AddWithValue("@Senha", textBox2.Text);
+                SqlDataReader DR;
+                try
+                {
+
+                    conexao.Open();
+                    DR = cmd.ExecuteReader();
+                    if (DR.Read())
+                    {
+                        string perfil = DR.GetValue(3).ToString();
+                        if (perfil == "admin")
+                        {
+                            Form2 principal = new Form2();
+                            principal.Show();
+                            principal.label1.Text = "Administrador";
+                            principal.panel1.BackColor = Color.DarkRed;
+                        }
+                        else
+                        {
+                            Form2 principal = new Form2();
+                            principal.Show();
+                            principal.label1.Text = "Usuário";
+                            principal.button1.Enabled = false;
+                            principal.panel1.BackColor = Color.Blue;
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuário e/ou Senha Inválido(s)");
+                    }
+                }
+                //caso ocorra algum erro
+                catch (Exception ex)
+                {
+                    //exiba qual é o erro
+                    MessageBox.Show(ex.Message);
+                }
+                // de qualquer forma sempre fechar a conexão com o banco ("lembrar da porta da geladeira rsrsrs")
+                finally
+                {
+                    conexao.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Projeto Login/Projeto Login/Usuarios.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Projeto_Login
+{
+    partial class Usuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelId = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.cboPerfil = new System.Windows.Forms.ComboBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(32, 46);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(16, 13);
+            this.labelId.TabIndex = 0;
+            this.labelId.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Login";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "senha";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(201, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Perfil";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(55, 38);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(59, 20);
+            this.txtId.TabIndex = 4;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(55, 73);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.TabIndex = 5;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(55, 108);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 6;
+            // 
+            // cboPerfil
+            // 
+            this.cboPerfil.FormattingEnabled = true;
+            this.cboPerfil.Items.AddRange(new object[] {
+            "admin",
+            "user"});
+            this.cboPerfil.Location = new System.Drawing.Point(242, 54);
+            this.cboPerfil.Name = "cboPerfil";
+            this.cboPerfil.Size = new System.Drawing.Size(112, 21);
+            this.cboPerfil.TabIndex = 7;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(19, 179);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 8;
+            this.btnCreate.Text = "Adicionar";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(101, 179);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 9;
+            this.btnRead.Text = "Pesquisar";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(183, 178);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Alterar";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(265, 179);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Remover";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // Usuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 250);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.cboPerfil);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelId);
+            this.Name = "Usuarios";
+            this.Text = "Usuarios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.ComboBox cboPerfil;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
