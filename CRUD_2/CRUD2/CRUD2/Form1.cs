19265a73805f2dc6cc70f9a765e81ff103d225d9
BRUNO IROKAWA  FERNANDES <RM12294@fiap.com.br>
2017-03-23 11:21:27 -0300

    Colocando parâmetros em projeto já existente de agenda

---
+++ CRUD_2/CRUD2/CRUD2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Importar o SQL Client
+using System.Data.SqlClient;
+
+namespace CRUD2
+{
+    /*SQL CLIENT
+     *      > Sql Connection
+     *      > Sql Command
+     *      > Sql Data Reader
+     */
+    public partial class Form1 : Form
+    {
+        // Classe principal (construção do form)
+        // Criar uma variável para armazenar a string de conexão
+        // Mudar para a barra (\) para 2 barras (\\)
+        static string strCn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\DBCRUD2.mdf;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CRUD_2/SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿create table tb_agenda(
+id int primary key,
+nome varchar(50),
+fone varchar(15)
+);
