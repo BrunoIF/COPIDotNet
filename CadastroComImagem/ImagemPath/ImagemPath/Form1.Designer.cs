5ff15b43bc172f58ff7827e56feca92133fbd7aa
logonrm <logonrm@fiap.com.br>
2017-04-27 11:18:23 -0300

    Alterações não salvas

--- CadastroComImagem/ImagemPath/ImagemPath/Form1.Designer.cs
+++ CadastroComImagem/ImagemPath/ImagemPath/Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.imgmImagem = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.imgmImagem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descrição da Imagem:";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(40, 56);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(248, 20);
+            this.txtDescricao.TabIndex = 1;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(40, 99);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(123, 23);
+            this.btnAdicionar.TabIndex = 2;
+            this.btnAdicionar.Text = "Adicionar Imagem";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(536, 18);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(97, 23);
+            this.btnListar.TabIndex = 3;
+            this.btnListar.Text = "Listar Imagens";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // imgmImagem
+            // 
+            this.imgmImagem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.imgmImagem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgmImagem.Location = new System.Drawing.Point(40, 128);
+            this.imgmImagem.Name = "imgmImagem";
+            this.imgmImagem.Size = new System.Drawing.Size(248, 226);
+            this.imgmImagem.TabIndex = 4;
+            this.imgmImagem.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(311, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(322, 298);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(645, 366);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.imgmImagem);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cadastro Imagem Path";
+            ((System.ComponentModel.ISupportInitialize)(this.imgmImagem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.PictureBox imgmImagem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- CadastroComImagem/ImagemPath/ImagemPath/Form1.cs
+++ CadastroComImagem/ImagemPath/ImagemPath/Form1.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+// Biblioteca para trabalhar com SQL
+using System.Data.SqlClient;
 
 namespace ImagemPath
 {
     public partial class Form1 : Form
     {
+        // string de conexão
+        static string strCn = "Data Source=sql.fiap.com.br;Initial Catalog=3EMIA;User ID=RM12294;Password=230200";
+        // Instanciando um objeto tendo como modelo a classe SqlConnection para conexão com o banco
+        SqlConnection conexao = new SqlConnection(strCn);
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            // instrução SQL responsável por inserir o caminho da imagem no banco de dados
+            string adicionar = "insert into tb_imagem_path_02(imgCODIGO,imgDESCRICAO,imgCAMINHO) values(@CODIGO, @DESCRICAO, @CAMINHO)";
+            // instanciando um objeto tendo como modelo a classe SqlCommand para executar a instrução SQL
+            SqlCommand cmd = new SqlCommand(adicionar, conexao);
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
         }
     }
 }
