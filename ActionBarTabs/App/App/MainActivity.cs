0c3ccb4e6567c8b2160a461891c4f60c46714dcc
logonrm <logonrm@fiap.com.br>
2017-08-30 12:24:44 -0300

    Navegação por Tabs

---
+++ ActionBarTabs/App/App/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace App
+{
+    [Activity(Label = "App", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        TextView txtValor;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            txtValor = FindViewById<TextView>(Resource.Id.tvValor);
+
+            ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+            // Adiciona as tabs no Action Bar
+            AddTab("Tab um");
+            AddTab("Tab dois");
+
+        }
+
+        private void AddTab(string tabTexto)
+        {
+            ActionBar.Tab tab = ActionBar.NewTab();
+            tab.SetText(tabTexto);
+
+            tab.TabSelected += OnTabSelected;
+            ActionBar.AddTab(tab);
+        }
+
+        private void OnTabSelected(object sender, ActionBar.TabEventArgs e)
+        {
+            var tabAtual = (ActionBar.Tab)sender;
+            if (tabAtual.Position == 0)
+            {
+                txtValor.Text = "Esta é a TAB UM que foi selecionada";
+            }
+            else
+            {
+                txtValor.Text = "Esta é a TAB DOIS que foi selecionada";
+            }
+
+        }
+    }
+}
+
