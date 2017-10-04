63f09ed70b53849aef0932cf048c1dccdfb2e79b
logonrm <logonrm@fiap.com.br>
2017-10-04 11:55:37 -0300

    projetoActionBarTabsListView

---
+++ projetoActionBarTabsListView/ActionBarTabsListView/ActionBarTabsListView/Abacaxi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ActionBarTabsListView
+{
+    [Activity(Label = "Abacaxi", NoHistory = true)]
+    public class Abacaxi : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            //Create yout application here
+            SetContentView(Resource.Layout.Abacaxi);
+        }
+    }
+}
---
+++ projetoActionBarTabsListView/ActionBarTabsListView/ActionBarTabsListView/MainActivity.cs
@@ -0,0 +1,84 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace ActionBarTabsListView
+{
+    [Activity(Label = "ActionBarTabsListView", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        TextView txtValor;
+        ListView lv;
+        string[] items;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            txtValor = FindViewById<TextView>(Resource.Id.textView1);
+            lv = (ListView)FindViewById(Resource.Id.lstDados);
+
+            ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+            //Adiciona as tabs no Action Bar
+            AddTab("Frutas");
+            AddTab("Verduras");
+            AddTab("Legumes");
+        }
+
+        private void AddTab(string tabTexto)
+        {
+            ActionBar.Tab tab = ActionBar.NewTab();
+            tab.SetText(tabTexto);
+            tab.TabSelected += OnTabSelected;
+            ActionBar.AddTab(tab);
+        }
+
+        private void OnTabSelected(object sender, ActionBar.TabEventArgs e)
+        {
+            var tabAtual = (ActionBar.Tab)sender;
+            if (tabAtual.Position == 0)
+            {
+                txtValor.Text = "Frutas";
+                items = new string[] { "Abacaxi", "Banana", "Maçã" };
+            }
+            else
+            {
+                if (tabAtual.Position == 1)
+                {
+                    txtValor.Text = "Verduras";
+                    items = new string[] { "Alface", "Brócolis", "Rúcula" };
+                }
+            }
+            else
+            {
+                    txtValor.Text = "Legumes";
+                    items = new string[] { "Abóbora", "Berinjela", "Pimentão" };
+            }
+
+            ArrayAdapter adaptador = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, items);
+            lv.Adapter = adaptador;
+            lv.ItemClick += OnListItemClick;
+        }
+
+        private void OnListItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            int valor = e.Position;
+            string texto = items[valor];
+
+            if (texto.Equals("Abacaxi"))
+            {
+                StartActivity(typeof(Abacaxi));
+            }
+
+            if (texto.Equals("Brócolis"))
+            {
+                StartActivity(typeof(Brocolis));
+            }
+        }
+    }
+
+}
+
